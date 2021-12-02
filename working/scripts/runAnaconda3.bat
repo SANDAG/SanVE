7e640d32c7752d0055110061625ff21b0a7ad2ea
esanchez01 <ens004@ucsd.edu>
2021-12-02 15:38:19 -0800

    Remove old working folder and initialize scripts folder

--- working/scripts/runAnaconda3.bat
+++
@@ -1,7 +0,0 @@
-set root=C:\ProgramData\Anaconda3
-call %root%\Scripts\activate.bat %root%
-rem call conda env create -f python\environment.yml
-rem call activate sensitivity
-jupyter notebook
-rem call conda deactivate
-rem pause
